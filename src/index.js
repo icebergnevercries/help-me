3728f81773055aef1fdf028c1a813d012eb21971
icebergnevercries <denislosik@mail.ru>
2018-10-07 23:31:58 +0300

    complete test 5/16

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function count(s, pairs) {
-  // your implementation
+  let N = 1;
+
+  for(let i = 0, len = Object.keys(pairs).length; i < len; i++){
+    N *= (pairs[i][0] ** pairs[i][1]);
+  }
+
+  let counter = (n) => {
+    let S = [];
+    for(let i = 0; i < n; i++){
+      S.push(1);
+    }
+    for(let i = 2; i <= n; i++){
+      if(n % i == 0){
+        for(let j = i; j <= n; j += i){
+          S[j - 1] = 0;
+        }
+      }
+    }
+    var count = 0;
+    for(var i = 1; i < n; i++){
+      if(S[i] == 0){
+        count++;
+      }
+    }
+    return count;
+  }  
+
+  if(s == '1' && N < 10000000){
+    return N - counter(N);
+  }
+
+  if(s == '0'){
+    return counter(N);
+  }
+
+  return answer;
 }
